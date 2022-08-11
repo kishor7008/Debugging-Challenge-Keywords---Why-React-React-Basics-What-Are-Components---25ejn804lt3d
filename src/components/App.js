990e54165030b163c07910b9473ceea2e9ba9e3b
kishor7008 <pspkbabul@gmail.com>
2022-08-11 08:46:16 +0530

    ceve

--- src/components/App.js
+++ src/components/App.js
@@ -9,9 +9,9 @@ const App = () => {
 
   
   return (
-    <div class="ball">
-      <h1 class="count" onDoubleClick={() => { alert("cant edit it") }}>{count}</h1>
-      <button class='increment-button' onClick={() => { setCount(count + 1) }}>Increment</button>
+    <div className="ball">
+      <h1 className="count" onDoubleClick={() => { alert("cant edit it") }}>{count}</h1>
+      <button className='increment-button' onClick={() => { setCount(count + 1) }}>Increment</button>
     </div>
   )
 }
