241b6adfb63a493d7a52195ab5f6dcabcaa6da67
kishor7008 <pspkbabul@gmail.com>
2022-08-18 09:16:27 +0530

    dws

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,8 @@ const App = () => {
   
   return (
     <div className="Main">
-    sdvdsv
+    <h1 data-ns-test attribute="project-name">parallel webpages</h1>
+    <div data-ns-test attribute="project-description">background image fixed when page scrool</div>
     </div>
   )
 }
